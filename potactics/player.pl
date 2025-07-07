d1626b479b51e930b44b5aa69320bd2ed10e1ae3
I bring dishonor to mother and father stones <201484118+Doljonijiarnimorinar@users.noreply.github.com>
2025-07-07 16:21:46 -0400

    Update player.pl (#48)
    Co-authored-by: djgouac <86606378+djgouac@users.noreply.github.com>

--- potactics/player.pl
+++ potactics/player.pl
@@ -1,21 +1,22 @@
 sub EVENT_CLICKDOOR {
 	# gm can always use the door
 	# used status > 79 because that is what bothunder/player.pl used
-	if($status > 79 && ($doorid == 2 || $doorid == 3 || $doorid == 4 || $doorid == 5)) {
-		quest::movepc(214,278,178,2); # Zone: potactics
+	# too bad we don't care about bothunder instead 50 for THJ - Twinkles
+	if($status >= 50 && ($doorid == 2 || $doorid == 3 || $doorid == 4 || $doorid == 5)) {
+		quest::MovePCInstance(214,$instanceid,278,178,2); # Zone: potactics
 	} else {
 		#vallon's doors
 		if($doorid == 2 || $doorid == 3) {
 			#check if Vallon is up
 			if(!$entity_list->IsMobSpawnedByNpcTypeID(214083)) {
-				quest::movepc(214,278,178,2); # Zone: potactics
+				quest::MovePCInstance(214,$instanceid,278,178,2); # Zone: potactics
 			}
 		}
 		#tallon's doors
 		if($doorid == 4 || $doorid == 5) {
 			#check if Tallon is up
 			if(!$entity_list->IsMobSpawnedByNpcTypeID(214026)) {
-				quest::movepc(214,278,178,2); # Zone: potactics
+				quest::MovePCInstance(214,$instanceid,278,178,2); # Zone: potactics
 			}
 		}
 		#Glykus_Helmir door
