2331ed25b9ee67784d2eac2248c5a744eeb2d541
I bring dishonor to mother and father stones <201484118+Doljonijiarnimorinar@users.noreply.github.com>
2025-08-11 18:19:34 -0400

    Update Anson_McBale.lua (#74)
    Co-authored-by: djgouac <86606378+djgouac@users.noreply.github.com>

--- highpasshold/Anson_McBale.lua
+++ highpasshold/Anson_McBale.lua
@@ -8,9 +8,9 @@ function event_say(e)
 			e.self:Emote("looks at you suspiciously. 'Yeah? Whacha want?'");
 		elseif(e.message:findi("see stanos") and level >= 50) then
 			e.self:Say("This better be important.");
-			eq.spawn2(5088,0,0,336,10,45,450); -- NPC: Stanos_Herkanor
+			eq.unique_spawn(5088,0,0,336,10,45,450); -- NPC: Stanos_Herkanor
 		elseif(e.message:findi("see stanos") and level <= 49) then
-			e.self:Say("Get lost. Stanos is a busy man, and you look like you could barely handle a Shralock orc.")
+			e.self:Say("Get lost. Stanos is a busy man, and you look like you could barely handle a Shralok orc.")
 		end
 	else
 		e.self:Say("Go away! We don't have time for the likes of you.");
@@ -18,8 +18,9 @@ function event_say(e)
 end
 
 function event_trade(e)
-	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 28014}) and e.other:GetLevel() >= 50) then
+	local item_lib = require("items")
+	if(e.other:GetLevel() >= 50) then
+		if(item_lib.check_turn_in(e.trade, {item1 = 28014})) then
 		--e.self:Say("Ah, we have been expecting this. Let me get Stanos, he will want to inspect it first, but here are your coins.");
 		e.self:Say("Ah, we have been expecting this. Let me get Stanos, he will want to inspect it.");
 		e.other:Ding();
@@ -31,7 +32,13 @@ function event_trade(e)
 		e.other:AddEXP(500);
 		-- e.other:Message(15,"You receive 35 platinum from Anson McBale.")
 		-- e.other:AddMoneyToPP(0, 0, 0, 35, true);
-		eq.spawn2(5088,0,0,336,10,45,450); -- NPC: Stanos_Herkanor
+		eq.unique_spawn(5088,0,0,336,10,45,450); -- NPC: Stanos_Herkanor
+		end
+	elseif(e.other:GetLevel() <= 49) then
+		if(item_lib.check_turn_in(e.trade, {item1 = 28014})) then
+			e.self:Say("What is this? I trust Vilnius' judgment, but you're not yet ready for what is to come. Come back when you have some more experience at your trade.");
+			e.other:SummonFixedItem(28014);
+		end
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
